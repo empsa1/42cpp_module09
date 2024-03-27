27f7e3d1d9ea8d0ac30d17738fbfffbd8796dd0c
Eduardo manuel Portela da silva <eportela@c2r10s5.42lisboa.com>
2024-03-27 11:59:03 +0000

    i think its done

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -8,38 +8,58 @@
 int inputParser(char *expression)
 {
     int i = -1;
-    std::string temp(expression);
-    if (temp.find_first_not_of(RPN_ARRAY) == std::string::npos)
+    int counter = 0;
+    std::string temp;
+    while (expression[++i] != '\0')
+    {
+        if (std::isdigit(expression[i]))
+            counter++;
+        if (std::isdigit(expression[i]) && std::isdigit(expression[i + 1]))
+        {
+            std::cout << RED << "Error: Invalid input around: " << expression[i] << expression[i + 1] << std::endl << "Input numbers cannot be greater or equal to 10" << RESET << std:: endl;
+            std::cout << "irst" << std::endl;
+            return FAILURE_ON_EXIT;
+        }
+    }
+    i = -1;
+    while (expression[++i] != '\0')
+    {
+        if (std::isspace(expression[i]))
+            continue ;
+        else
+            temp.append(&expression[i], 1);
+    }
+    if (temp.find_first_not_of(RPN_ARRAY) != std::string::npos)
     {
         std::cout << RED << "Error: Invalid characters found inside argument!" << RESET << std::endl;
         return FAILURE_ON_EXIT;
     }
-    while (expression[++i] != '\0')
+    if (counter < 2)
     {
-        if (std::isdigit(expression[i]) && std::isdigit(expression[i + 1]))
-        {
-            std::cout << RED << "Error: Invalid input aroud: " << expression[i] << expression[i + 1] << std::endl << "Input numbers cannot be greater or equal to 10" << RESET << std:: endl;
-            return FAILURE_ON_EXIT;
-        }
-            
+        std::cout << RED << "Error: Too few arguments!" << RESET << std::endl;
+        return FAILURE_ON_EXIT;
     }
     return IS_VALID_INPUT;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc == 2 && inputParser(argv[1]) != -1)
+    if (argc == 2)
     {
-        try
+        if (inputParser(argv[1]) != -1)
         {
-            RPN(std::string(argv[1]));
+            try
+            {
+                RPN(std::string(argv[1]));
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+            return SUCESS_ON_EXIT;
         }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
-        return SUCESS_ON_EXIT; 
     }
-    std::cout << RED << INVALID_ARGUMENTS << RESET << std::endl;
+    else
+        std::cout << RED << INVALID_ARGUMENTS << RESET << std::endl;
     return FAILURE_ON_EXIT;
 }
